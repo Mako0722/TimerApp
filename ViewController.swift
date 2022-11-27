5d3567243b280937f5e968ba0dc0a85e50bdc42b
Mako0722 <zhongtiancheng79@gmail.com>
2022-11-27 21:46:25 +0900

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimerApp
+//
+//  Created by 中田誠 on 2022/11/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
